a87844c377bc9bfa8312a91d420a511e4a54c3a9
Kainoa Aqui <aquic@spu.edu>
2025-06-12 16:12:45 -0700

    Solution for contains duplicate

---
+++ 01_arrays_and_hashing/contains_dup.py
@@ -0,0 +1,16 @@
+class Solution:
+    def hasDuplicate(self, nums: List[int]) -> bool:
+
+        hashset = set()
+
+        for i in nums:
+            if i in hashset:
+                return True
+            
+            hashset.add(i)
+        
+        return False
+
+
+        # Time Complexity: O(n)
+        # Space Complexity: O(n)
